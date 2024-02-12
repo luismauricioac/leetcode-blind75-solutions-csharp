54b25623ddd9c25aa1b95ec2e962cf0222ba0ac7
Luis Alamo <luismalamoc@gmail.com>
2024-02-12 15:20:56 -0300

    Add project files.

---
+++ leetcode-blind75-solutions-csharp/Solutions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace leetcode_blind75_solutions_csharp
+{
+    public class Solutions
+    {
+        public bool IsPalindrome(String s)
+        {
+            String strCleaned = Regex.Replace(s.ToLower(), "[^a-zA-Z0-9]", "");
+            int last = strCleaned.Length - 1;
+            for (int i = 0; i < strCleaned.Length; i++)
+            {
+
+                if (!(strCleaned[i] == strCleaned[last]))
+                {
+                    return false;
+                }
+                last -= 1;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcode-blind75-solutions-csharpTests/SolutionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_blind75_solutions_csharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_blind75_solutions_csharp.Tests
+{
+    [TestClass()]
+    public class SolutionsTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            Solutions solutions = new Solutions();
+            Assert.IsTrue(solutions.IsPalindrome("amanaplanacanalpanama"));
+        }
+    }
+}
