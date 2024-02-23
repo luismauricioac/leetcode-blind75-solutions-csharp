86986e07381159f6dbe4009f93ab3e9377bb30db
Luis Alamo <luismalamoc@gmail.com>
2024-02-23 12:25:06 -0300

    feat: twoSum, lengthOfLongestSubstring were added

--- leetcode-blind75-solutions-csharp/Solutions.cs
+++ leetcode-blind75-solutions-csharp/Solutions.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace leetcode_blind75_solutions_csharp
 {
     public class Solutions
     {
-        public bool IsPalindrome(String s)
+        private Solutions() { }
+        public static bool IsPalindrome(String s)
         {
             String strCleaned = Regex.Replace(s.ToLower(), "[^a-zA-Z0-9]", "");
             int last = strCleaned.Length - 1;
@@ -23,6 +20,42 @@ namespace leetcode_blind75_solutions_csharp
                 last -= 1;
             }
             return true;
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> prevMap = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (prevMap.ContainsKey(diff))
+                {
+                    return new int[] { prevMap[diff], i };
+                }
+                prevMap.Add(nums[i], i);
+
+            }
+            return new int[] { };
+        }
+
+        public static int LengthOfLongestSubstring(String s)
+        {
+            HashSet<char> charsSet = new HashSet<char>();
+            int l = 0;
+            int res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char aux = s[i];
+                while (charsSet.Contains(aux))
+                {
+                    char rem = s[l];
+                    charsSet.Remove(rem);
+                    l += 1;
+                }
+                charsSet.Add(aux);
+                res = Math.Max(res, (i - l + 1));
+            }
+            return res;
         }
     }
 }
--- leetcode-blind75-solutions-csharpTests/SolutionsTests.cs
+++ leetcode-blind75-solutions-csharpTests/SolutionsTests.cs
@@ -14,8 +14,22 @@ namespace leetcode_blind75_solutions_csharp.Tests
         [TestMethod()]
         public void IsPalindromeTest()
         {
-            Solutions solutions = new Solutions();
-            Assert.IsTrue(solutions.IsPalindrome("amanaplanacanalpanama"));
+            Assert.IsTrue(Solutions.IsPalindrome("amanaplanacanalpanama"));
+        }
+
+        [TestMethod()]
+        public void TwoSum()
+        {
+            int[] expeted = new int[] { 0, 1 };
+            int[] testResult = Solutions.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            CollectionAssert.AreEqual(expeted, testResult);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring()
+        {
+            int expeted = 3;
+            Assert.AreEqual(expeted, Solutions.LengthOfLongestSubstring("pwwkew"));
         }
     }
 }
